1e19c416628db021ec9bce47513d76014d2e2fd7
ssyamsulhadii <c.syamsulhadi.si@gmail>
2025-11-17 20:15:20 +0700

    Menghapus placeholder

--- resources/views/pages/usul-pensiun/identitas-diri-form.blade.php
+++ resources/views/pages/usul-pensiun/identitas-diri-form.blade.php
@@ -3,13 +3,13 @@
         <x-forms.selection-group name='jenis_pensiun_id' label='Jenis Pensiun'
             item="{{ $pegawai->jenis_pensiun_id ?? '' }}" :items="$list_jenis_pensiun" />
         <x-forms.input name="nama" label="Nama Lengkap <small class='text-danger'>Dengan Gelar</small>"
-            item="{{ $pegawai->nama ?? '' }}" phr="AHMAD YADI, S.Kom., M.Kom"></x-forms.input>
+            item="{{ $pegawai->nama ?? '' }}"></x-forms.input>
         <x-forms.input name="nip" label="Nomor Induk Pegawai (NIP) <small class='text-danger'>Tanpa Spasi</small>"
-            item="{{ $pegawai->nip ?? '' }}" type="number" phr="******************"></x-forms.input>
-        <x-forms.input name="nkk" label="Nomor Kartu Keluarga (NKK)" item="{{ $pegawai->nkk ?? '' }}" type="number"
-            phr="****************"></x-forms.input>
+            item="{{ $pegawai->nip ?? '' }}" type="number"></x-forms.input>
+        <x-forms.input name="nkk" label="Nomor Kartu Keluarga (NKK)" item="{{ $pegawai->nkk ?? '' }}"
+            type="number"></x-forms.input>
         <x-forms.input name="nik" label="Nomor Induk Kependudukan (NIK)" item="{{ $pegawai->nik ?? '' }}"
-            type="number" phr="****************"></x-forms.input>
+            type="number"></x-forms.input>
         <x-forms.input name="tanggal_lahir" label="Tanggal Lahir"
             item="{{ $pegawai?->tanggal_lahir->format('Y-m-d') ?? '' }}" type="date"></x-forms.input>
         <x-forms.selection-group name='jenis_kelamin' label='Jenis Kelamin' item="{{ $pegawai->jenis_kelamin ?? '' }}"
@@ -22,16 +22,16 @@
             item="" type="date"></x-forms.input>
         <x-forms.selection-group name='jabatan_nominatif_id' label='Jabatan Nominatif'
             item="{{ $pegawai->jabatan_nominatif_id ?? '' }}" :items="$list_jabatan_nominatif" />
-        <x-forms.input name="unit_organisasi" label="Unit Organisasi" item="{{ $pegawai->unit_organisasi ?? '' }}"
-            phr="Nama Kantor/Sekolah"></x-forms.input>
+        <x-forms.input name="unit_organisasi" label="Unit Organisasi"
+            item="{{ $pegawai->unit_organisasi ?? '' }}"></x-forms.input>
         <x-forms.input name="jabatan" label="Jabatan" item="{{ $pegawai->jabatan ?? '' }}"
             phr="Nama Jabatan Struktural/Fungsional/Pelaksana"></x-forms.input>
 
         <x-forms.selection-group name='pangkat_golongan' label='Pangkat Golongan'
             item="{{ $pegawai->pangkat_golongan ?? '' }}" :items="$pil_pangkat_golongan" />
 
-        <x-forms.input name="telpon" label="Telpon" item="{{ $pegawai->telpon ?? '' }}" type="number"
-            phr="0812XXXXXXXX"></x-forms.input>
+        <x-forms.input name="telpon" label="Telpon" item="{{ $pegawai->telpon ?? '' }}"
+            type="number"></x-forms.input>
         <x-forms.textarea name="alamat_pensiun" label="Alamat Pensiun" item="{{ $pegawai->alamat_pensiun ?? '' }}"
             rows="3"></x-forms.textarea>
     </div>
